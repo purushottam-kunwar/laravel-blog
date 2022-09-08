736c732753811fe1f05a659874711b0b6a4561da
Purushottam Kunwar <107859674+pk-savitriya@users.noreply.github.com>
2022-09-08 11:50:20 +0545

    added some testing

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\Post;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
@@ -13,6 +14,6 @@ class ExampleTest extends TestCase
      */
     public function test_that_true_is_true()
     {
-        $this->assertTrue(true);
+        Post::archives();
     }
 }
